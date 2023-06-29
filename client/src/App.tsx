db36a558f6af9408db8303bfba9cf9b5e6266740
clouderMark <charnysheuski@yandex.by>
2023-06-29 15:17:57 +0300

    feat: add CssBaseline

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {CssBaseline} from '@mui/material';
 import {BrowserRouter} from 'react-router-dom';
 import AppRouter from './components/AppRouter';
 
 function App() {
   return (
     <BrowserRouter>
+      <CssBaseline />
       <AppRouter />
     </BrowserRouter>
   );
