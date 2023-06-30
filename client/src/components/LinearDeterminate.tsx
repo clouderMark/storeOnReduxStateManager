ff5f061f80a0b8d3df95370080fb7d0813652f94
clouderMark <charnysheuski@yandex.by>
2023-06-30 15:00:15 +0300

    feat: add loader component

---
+++ client/src/components/LinearDeterminate.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import {Box, LinearProgress} from '@mui/material/';
+import {theme} from '../styles/theme';
+
+const LinearDeterminate = () => {
+  const [progress, setProgress] = React.useState(0);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((oldProgress) => {
+        if (oldProgress === 100) {
+          return 0;
+        }
+
+        const diff = Math.random() * 10;
+
+        return Math.min(oldProgress + diff, 100);
+      });
+    }, 500);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        '& .MuiLinearProgress-colorPrimary': {
+          backgroundColor: 'transparent',
+        },
+        '& .MuiLinearProgress-barColorPrimary': {
+          backgroundColor: theme.palette.first.main,
+        },
+      }}
+    >
+      <LinearProgress variant="determinate" value={progress} />
+    </Box>
+  );
+};
+
+export default LinearDeterminate;
