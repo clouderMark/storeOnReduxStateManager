316d71ada4e6fb2a0e241d7331af430fb97aa96f
clouderMark <charnysheuski@yandex.by>
2023-06-30 15:03:35 +0300

    feat add fetch basket and show loader while data is fetching

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
 import {CssBaseline} from '@mui/material';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import AppRouter from './components/AppRouter';
 import {useCheckUserMutation} from './redux/userApi';
 import {useAppDispatch} from './redux/hooks';
 import {login, logout} from './redux/userSlice';
+import {useFetchBasketMutation} from './redux/basketApi';
+import {setProds} from './redux/basketSlice';
+import Loader from './components/LinearDeterminate';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
   const dispatch = useAppDispatch();
   const [checkUser, {data: checkUserData, isSuccess: isCheckUserSuccess, isError: isCheckUserError}] =
     useCheckUserMutation();
-
+  const [fetchBasket, {data: basketData, isSuccess: isBasketSuccess}] = useFetchBasketMutation();
   const token = localStorage.getItem('token');
 
   useEffect(() => {
@@ -22,6 +26,7 @@ const App = () => {
   useEffect(() => {
     if (isCheckUserSuccess) {
       dispatch(login({token: checkUserData!.token}));
+      if (isBasketSuccess && loading) setLoading(false);
     }
   }, [isCheckUserSuccess]);
 
@@ -30,6 +35,21 @@ const App = () => {
       dispatch(logout());
     }
   }, [isCheckUserError]);
+
+  useEffect(() => {
+    fetchBasket();
+  }, []);
+
+  useEffect(() => {
+    if (isBasketSuccess) {
+      dispatch(setProds(basketData!.products));
+      if (isCheckUserSuccess && loading) setLoading(false);
+    }
+  }, [isBasketSuccess]);
+
+  if (loading) {
+    return <Loader />;
+  }
 
   return (
     <BrowserRouter>
