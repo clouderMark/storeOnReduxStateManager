b67e3fa29085a561aee23e781d154094bcdc6d5f
clouderMark <charnysheuski@yandex.by>
2023-07-05 15:01:23 +0300

    feat: add interfaces

--- client/src/interfaces/interfaces.ts
+++ client/src/interfaces/interfaces.ts
@@ -2,6 +2,14 @@ interface IId {
   id: number;
 }
 
+export interface IIdAndName extends IId {
+  name: string;
+}
+
+interface IIdNameImage extends IIdAndName {
+  image: string;
+}
+
 export interface ICustomError {
   data: {
     message: string;
@@ -9,13 +17,68 @@ export interface ICustomError {
   status: number;
 }
 
-interface IItem extends IId {
-  name: string;
+export interface IItem extends IIdNameImage {
   price: number;
   quantity: number;
-  image: string;
 }
 
 export interface IBasket extends IId {
   products: IItem[] | [];
+}
+
+export interface IListItem extends IId {
+  value: string;
+}
+
+export interface IInfo extends IId {
+  image: string;
+  listTitle: string;
+  title: string;
+  header: string;
+  listItems: IListItem[];
+  paragraphs: IListItem[];
+}
+
+interface IOpinion extends IIdNameImage {
+  title: string;
+  email: string;
+  fax: string;
+  listTitle: string;
+  phone: string;
+  listItems: IListItem[];
+  paragraphs: IListItem[];
+}
+
+export interface IAreaResponse extends IIdAndName {
+  cardImage: string;
+  headerImage: string;
+  title: string;
+  paragraphs: IListItem[];
+  industryId?: number;
+  info: IInfo;
+  opinion: IOpinion;
+  sliderImage?: string;
+}
+
+export interface IProperty extends IIdAndName {
+  productId: number;
+  value: string;
+}
+
+export interface IProductWithProps extends IProduct {
+  solution: IIdAndName;
+  industry: IIdAndName;
+  area: IIdAndName;
+  props: IProperty[];
+}
+
+export interface IProduct extends IIdAndName {
+  solutionId: number;
+  industryId: number;
+  areaId: number;
+  image: string | null;
+  price: number;
+  rating: number;
+  article: number;
+  weight: number;
 }
