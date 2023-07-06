4fd57a9eca797c5c04af262483c661c4d4538ff8
clouderMark <charnysheuski@yandex.by>
2023-07-06 15:34:46 +0300

    fix, feat: add endpoint for fetch solutions and fix request addresses

--- client/src/redux/catalogApi.ts
+++ client/src/redux/catalogApi.ts
@@ -1,5 +1,5 @@
 import {createApi, fetchBaseQuery, BaseQueryFn, FetchArgs} from '@reduxjs/toolkit/query/react';
-import {ICustomError, IAreaResponse} from '../interfaces/interfaces';
+import {ICustomError, IAreaResponse, IIdAndName} from '../interfaces/interfaces';
 
 interface IData {
   body: FormData;
@@ -11,14 +11,14 @@ const BASE_URL = process.env.REACT_APP_API_URL;
 export const catalogApi = createApi({
   reducerPath: 'catalogApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: `${BASE_URL}industry`,
+    baseUrl: `${BASE_URL}`,
     credentials: 'include',
   }) as BaseQueryFn<string | FetchArgs, unknown, ICustomError, {}>,
   endpoints: (builder) => ({
     createIndustry: builder.query<IAreaResponse, IData>({
       query: (data) => {
         const req: FetchArgs = {
-          url: '/create',
+          url: '/industry/create',
           method: 'POST',
           body: {...data.body},
         };
@@ -30,17 +30,24 @@ export const catalogApi = createApi({
     }),
     getIndustries: builder.query<IAreaResponse[], void>({
       query: () => ({
-        url: '/getall',
+        url: '/industry/getall',
         method: 'GET',
       }),
     }),
     getSubIndustries: builder.query<IAreaResponse[], void>({
       query: () => ({
-        url: '/getall',
+        url: '/subindustry/getall',
+        method: 'GET',
+      }),
+    }),
+    getSolutions: builder.query<IIdAndName[], void>({
+      query: () => ({
+        url: '/solution/getall',
         method: 'GET',
       }),
     }),
   }),
 });
 
-export const {useCreateIndustryQuery, useGetIndustriesQuery, useGetSubIndustriesQuery} = catalogApi;
+export const {useCreateIndustryQuery, useGetIndustriesQuery, useGetSubIndustriesQuery, useGetSolutionsQuery} =
+  catalogApi;
