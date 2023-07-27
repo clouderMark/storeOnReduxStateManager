76faf282f54e713e3b51c29e303582592203ef35
clouderMark <charnysheuski@yandex.by>
2023-07-27 15:12:03 +0300

    feat: add reducers and use components

--- client/src/components/EditIndustry/EditIndustry.tsx
+++ client/src/components/EditIndustry/EditIndustry.tsx
@@ -1,15 +1,37 @@
-import {ChangeEvent, FormEvent, useEffect} from 'react';
-import {Box, Button, DialogActions, TextField, Typography} from '@mui/material';
+import {FormEvent, useEffect} from 'react';
+import {Box, Button, DialogActions, Typography} from '@mui/material';
 import {useAppDispatch, useAppSelector} from '../../redux/hooks';
-import {reset, selectEditIndustry, setCardImage, setName, setSliderImage} from '../../redux/editIndustrySlice';
+import {
+  appendParagraph,
+  changeParagraph,
+  removeParagraph,
+  reset,
+  selectEditIndustry,
+  setCardImage,
+  setHeaderImage,
+  setName,
+  setSliderImage,
+  setTitle,
+} from '../../redux/editIndustrySlice';
 import {useCreateIndustryMutation, useGetIndustryMutation, useUpdateIndystryMutation} from '../../redux/catalogApi';
 import DialogWithTitle from '../DialogWithTitle';
 import {selectDialogWithTitle, setShow} from '../../redux/dialogWithTitleSlice';
 import {selectUser} from '../../redux/userSlice';
 import CardInputImage from '../CardInputImage/CardInputImage';
+import CardInputImageText from '../CardInputImageText';
+import AppendInput from '../AppendInput';
 
 const EditIndustry = () => {
-  const {id, name, valid, cardImageUrl, sliderImageUrl} = useAppSelector(selectEditIndustry);
+  const {
+    id,
+    name,
+    valid,
+    cardImageUrl,
+    sliderImageUrl,
+    headerImageUrl,
+    title: headerTitle,
+    paragraphs,
+  } = useAppSelector(selectEditIndustry);
   const {title} = useAppSelector(selectDialogWithTitle);
   const dispatch = useAppDispatch();
   const {token} = useAppSelector(selectUser);
@@ -53,16 +75,12 @@ const EditIndustry = () => {
             <Box display={'flex'}>
               <Typography sx={{mr: '10px', writingMode: 'vertical-rl'}}>Карточка с названием</Typography>
               <Box sx={{width: '335px'}}>
-                <CardInputImage value={cardImageUrl} action={setCardImage} />
-                <TextField
-                  value={name}
-                  onChange={(e: ChangeEvent<HTMLInputElement>) => dispatch(setName(e.target.value))}
-                  required
-                  error={valid === false}
-                  color={valid ? 'success' : 'primary'}
-                  placeholder={'Название...'}
-                  className="mb-3"
-                  sx={{width: '100%'}}
+                <CardInputImageText
+                  imageValue={cardImageUrl}
+                  imageAction={setCardImage}
+                  textValue={name}
+                  textAction={setName}
+                  textValid={valid}
                 />
               </Box>
             </Box>
@@ -70,6 +88,21 @@ const EditIndustry = () => {
               <Typography sx={{mr: '10px', writingMode: 'vertical-rl'}}>Изображние для слайдера</Typography>
               <CardInputImage value={sliderImageUrl} action={setSliderImage} sx={{flexGrow: 1}} />
             </Box>
+          </Box>
+          <Box>
+            <CardInputImageText
+              imageValue={headerImageUrl}
+              imageAction={setHeaderImage}
+              textValue={headerTitle}
+              textAction={setTitle}
+            />
+            <AppendInput
+              title="Абзацы в шапке"
+              inputs={paragraphs}
+              appendAction={appendParagraph}
+              changeAction={changeParagraph}
+              deleteAction={removeParagraph}
+            />
           </Box>
           <DialogActions>
             <Button type="submit" variant="outlined">
--- client/src/redux/editIndustrySlice.ts
+++ client/src/redux/editIndustrySlice.ts
@@ -1,6 +1,8 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import uuid from 'react-uuid';
 import type {RootState} from './store';
 import {catalogApi} from './catalogApi';
+import {IParagraphs} from '../interfaces/interfaces';
 
 export enum EType {
   id = 'id',
@@ -14,6 +16,12 @@ export enum EType {
 
   sliderImage = 'sliderImage',
   sliderImageUrl = 'sliderImageUrl',
+
+  headerImage = 'headerImage',
+  headerImageUrl = 'headerImageUrl',
+
+  title = 'title',
+  paragraphs = 'paragraphs',
 }
 
 interface IInitialState {
@@ -27,6 +35,13 @@ interface IInitialState {
 
   [EType.sliderImage]: File | null;
   [EType.sliderImageUrl]: string;
+
+  [EType.headerImage]: File | null;
+  [EType.headerImageUrl]: string;
+
+  [EType.title]: string;
+
+  [EType.paragraphs]: IParagraphs[];
 }
 
 const initialState: IInitialState = {
@@ -40,6 +55,12 @@ const initialState: IInitialState = {
 
   [EType.sliderImage]: null,
   [EType.sliderImageUrl]: '',
+
+  [EType.headerImage]: null,
+  [EType.headerImageUrl]: '',
+
+  [EType.title]: '',
+  [EType.paragraphs]: [],
 };
 
 export const editIndustrySlice = createSlice({
@@ -59,6 +80,10 @@ export const editIndustrySlice = createSlice({
         state[EType.cardImageUrl] = URL.createObjectURL(file);
       }
     },
+    setName: (state, action: PayloadAction<IInitialState[EType.name]>) => {
+      state[EType.name] = action.payload;
+      state[EType.valid] = state[EType.name] !== '';
+    },
     setSliderImage: (state, action: PayloadAction<FileList | null>) => {
       const files = action.payload;
 
@@ -69,9 +94,35 @@ export const editIndustrySlice = createSlice({
         state[EType.sliderImageUrl] = URL.createObjectURL(file);
       }
     },
-    setName: (state, action: PayloadAction<IInitialState[EType.name]>) => {
-      state[EType.name] = action.payload;
-      state[EType.valid] = state[EType.name] !== '';
+
+    setHeaderImage: (state, action: PayloadAction<FileList | null>) => {
+      const files = action.payload;
+
+      if (files) {
+        const file = files[0];
+
+        state[EType.headerImage] = file;
+        state[EType.headerImageUrl] = URL.createObjectURL(file);
+      }
+    },
+
+    setTitle: (state, action: PayloadAction<IInitialState[EType.title]>) => {
+      state[EType.title] = action.payload;
+    },
+
+    appendParagraph: (state) => {
+      state[EType.paragraphs].push({id: null, value: '', unique: uuid()});
+    },
+    removeParagraph: (state, action: PayloadAction<string>) => {
+      state[EType.paragraphs] = state[EType.paragraphs].filter((elem) => elem.unique !== action.payload);
+    },
+    // prettier-ignore
+    changeParagraph: (state, action: PayloadAction<{value: string, unique: string}>) => {
+      state[EType.paragraphs] = state[EType.paragraphs].map((item) => (item.unique === action.payload.unique
+        ?
+        {...item, value: action.payload.value}
+        :
+        item));
     },
 
     [EType.reset]: () => initialState,
@@ -79,14 +130,21 @@ export const editIndustrySlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addMatcher(catalogApi.endpoints.getIndustry.matchFulfilled, (state, action) => {
-        state[EType.name] = action.payload[EType.name];
-        state[EType.valid] = action.payload[EType.name] !== '';
-        state[EType.cardImageUrl] = action.payload[EType.cardImage]
-          ? process.env.REACT_APP_IMG_URL + action.payload[EType.cardImage]
+        const {payload} = action;
+
+        state[EType.name] = payload[EType.name];
+        state[EType.valid] = payload[EType.name] !== '';
+        state[EType.cardImageUrl] = payload[EType.cardImage]
+          ? process.env.REACT_APP_IMG_URL + payload[EType.cardImage]
           : '';
-        state[EType.sliderImageUrl] = action.payload[EType.sliderImage]
-          ? process.env.REACT_APP_IMG_URL + action.payload[EType.sliderImage]
+        state[EType.sliderImageUrl] = payload[EType.sliderImage]
+          ? process.env.REACT_APP_IMG_URL + payload[EType.sliderImage]
           : '';
+        state[EType.title] = payload[EType.title];
+        state[EType.headerImageUrl] = payload[EType.headerImage]
+          ? process.env.REACT_APP_IMG_URL + payload[EType.headerImage]
+          : '';
+        state[EType.paragraphs] = payload[EType.paragraphs].map((item) => ({...item, unique: uuid()}));
       })
       .addMatcher(catalogApi.endpoints.createIndustry.matchFulfilled, () => initialState)
       .addMatcher(catalogApi.endpoints.updateIndystry.matchFulfilled, () => initialState);
@@ -95,4 +153,15 @@ export const editIndustrySlice = createSlice({
 
 export const selectEditIndustry = (state: RootState) => state.editIndustry;
 
-export const {setId, setCardImage, setName, setSliderImage, reset} = editIndustrySlice.actions;
+export const {
+  setId,
+  setCardImage,
+  setName,
+  setSliderImage,
+  setHeaderImage,
+  setTitle,
+  appendParagraph,
+  removeParagraph,
+  changeParagraph,
+  reset,
+} = editIndustrySlice.actions;
